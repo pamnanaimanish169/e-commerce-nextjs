74f776b3ca78e193c5d08c7ffe606105de07607d
Manish Pamnani <manishpamnani169@gmail.com>
2023-03-31 11:27:44 +0530

    Refactor: Clean up index.jsx :recycle:

--- src/pages/index.js
+++ src/pages/index.js
@@ -102,42 +102,20 @@ export default function Home() {
             {/* <!-- Hero Section --> */}
             <div className={styles.heroSection}>
               <div>
-                <div className={`row ${styles.exploreRow}`} style={{ marginBottom: "25px" }}>
+                <div className={`row ${styles.exploreRow}`}>
                   <div className="col-md-12 col-lg-6 col-xl-6">
                     <div className="row">
                       <h1 className={styles.exploreHeading}>
                         LET'S EXPLORE UNIQUE GADGETS
                       </h1>
                     </div>
-                    <div className="row"
-                      style={{
-                        fontFamily: "Poppins",
-                        fontSize: "32px",
-                        fontWeight: "400",
-                        lineHeight: "48px",
-                        letterSpacing: "-0.05em",
-                        textAlign: "left",
-                        paddingTop: "32px",
-                        paddingLeft: "12px"
-                      }}
-                    >
+                    <div className={styles.heroSubHeading}>
                       Live for Influential and Innovative fashion!
                     </div>
-                    <div className="row" style={{
-                      paddingTop: "32px", textAlign: "center"
-                    }}>
-                      <span className="col-xs-12 col-md-8 p-0" style={{
-                        marginBottom: "15px"
-                      }}>
-                        <button style={{
-                          fontFamily: "Poppins",
-                          fontSize: "20px",
-                          fontWeight: "400",
-                          background: "black",
-                          color: "white",
-                          padding: "10px",
-                          borderRadius: "10px",
-                        }} onClick={handleShopNow}>Shop Now</button>
+                    {/*  */}
+                    <div className={`row ${styles?.shopNowWrapper}`}>
+                      <span className="col-xs-12 col-md-8 p-0 my-2">
+                        <button className={styles.shopNowBtn} onClick={handleShopNow}>Shop Now</button>
                       </span>
                     </div>
                   </div>
@@ -151,9 +129,7 @@ export default function Home() {
             {/* <!-- Hero Section --> */}
 
             {/* <!-- Brands --> */}
-            <div style={{
-              background: "#EBD96B", display: "flex", justifyContent: "space-between", overflow: "auto", marginTop: "63px"
-            }} className={styles.brands}>
+            <div className={`d-flex justify-content-between ${styles.brands}`}>
               <div className="m-5">
                 <img src="./brand-1-bg-removed.png" alt="brand" />
               </div>
@@ -179,30 +155,22 @@ export default function Home() {
               </div>
             </div>
             {/* {/* <!-- Brands -->  */}
+
+            {/* <!-- New arrivals --> */}
             <div className='row p-5'>
               <div className={`py-5 ${styles.heading}`}>NEW ARRIVALS</div>
               {
-                productList?.filter((element) => element?.category === 'laptops').map((element, index) => (
+                productList?.filter((element) => element?.category === 'laptops').map((element) => (
                   <>
                     <div className="col-md-12 col-lg-6 col-xl-6 col-xxl-4">
                       <div>
-                        <img src={element?.thumbnail} alt="arrivals" style={{
-                          width: "100%",
-                          borderRadius: "50px",
-                          height: "400px"
-                        }} />
+                        <img src={element?.thumbnail} alt="arrivals" className={styles.arrivalsThumbnail} />
                         <div className='d-flex justify-content-between'>
                           <div>
-                            <div style={{
-                              fontFamily: 'POPPINS', fontSize: '32px', fontWeight: '500', lineHeight: '50px',
-                              letterSpacing: '-0.04em', textAlign: 'left',
-                            }}>
+                            <div className={styles.arrivalssubTitle}>
                               LAPTOPS & PC
                             </div>
-                            <div style={{
-                              fontFamily: 'POPPINS', fontSize: '24px', fontWeight: '500', lineHeight: '50px',
-                              letterSpacing: '-0.04em', textAlign: 'left',
-                            }}>
+                            <div className={styles.arrivalssubTitle}>
                               Explore Now!
                             </div>
                           </div>
@@ -222,7 +190,7 @@ export default function Home() {
                 {/* hero-image */}
                 <img src="./hero-image-3.png" alt="banner" className={styles.heroImage} />
               </div>
-              <div className={styles.bannerContent} style={{ margin: "auto" }}>
+              <div className={styles.bannerContent}>
                 <p className={styles.bannerHeading}>
                   PAYDAY SALE NOW
                 </p>
@@ -239,16 +207,7 @@ export default function Home() {
                 </p>
 
                 <p className={styles.bannerText}>
-                  <button style={{
-                    background: "black",
-                    color: "white",
-                    borderRadius: "10px",
-                    border: "none",
-                    paddingLeft: "20px",
-                    paddingRight: "20px",
-                    paddingTop: "5px",
-                    paddingBottom: "5px",
-                  }} onClick={() => navigateTo('../productListing/all')}>SHOP NOW</button>
+                  <button className={styles.bannerBtn} onClick={() => navigateTo('../productListing/all')}>SHOP NOW</button>
                 </p>
               </div>
             </div>
@@ -259,88 +218,28 @@ export default function Home() {
               <div className={`py-5 ${styles.heading}`}>Young’s Favourite</div>
               {productList?.filter((element) => element?.category === 'smartphones').map((element) => (
                 <div className="col-xs-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <div>
-                    <img src={element?.thumbnail} alt="arrivals" style={{
-                      width: "100%", borderRadius: "50px",
-                      height: "400px", objectFit: 'cover'
-                    }} />
-                    <div className='d-flex justify-content-between'>
-                      <div>
-                        <div style={{
-                          fontFamily: "Poppins, sans-serif",
-                          fontSize: "32px",
-                          fontWeight: "500",
-                          lineHeight: "50px",
-                          letterSpacing: "-0.04em",
-                          textAlign: "left",
-                        }}>
-                          {element?.title}
-                        </div>
-                        <div style={{
-                          fontFamily: "Poppins, sans-serif",
-                          fontSize: "24px",
-                          fontWeight: "500",
-                          lineHeight: "50px",
-                          letterSpacing: "-0.04em",
-                          textAlign: "left",
-                          color: "#7F7F7F"
-                        }}>
-                          Explore Now!
-                        </div>
-                      </div>
-                      <img src="./arrow-1.svg" alt="arrow" onClick={() => navigateTo('smartphones')} />
-                    </div>
+                  <div className= {`m-4 ${styles.arrivals}`} >
+                    <img src={element?.thumbnail} alt="arrivals" className={styles.favouriteThumbnail} />
+                    <div className={styles.favouriteTitle}>{element?.title}</div>
                   </div>
                 </div>
               ))}
             </div>
             {/* <!-- Young’s Favourite --> */}
 
-            
-
-            <div style={{
-              background: '#E5C643',
-              height: 'fit-content'
-            }}>
+            {/* Newsletter */}
+            <div className={styles.newsletterWrapper}>
               <div className="container py-5">
                 <div>
-                  <div style=
-                    {{
-                      fontFamily: 'Poppins',
-                      fontSize: '50px',
-                      fontWeight: '900',
-                      lineHeight: '70px',
-                      letterSpacing: '0.03em',
-                      textAlign: 'center',
-                    }}
-                  >JOIN SHOPPING COMMUNITY TO GET MONTHLY PROMO</div>
-                  <div style={{
-                    fontFamily: 'Poppins',
-                    fontSize: '32px',
-                    fontWeight: '400',
-                    lineHeight: '35px',
-                    letterSpacing: '0.03em',
-                    textAlign: 'center',
-                  }} className="m-2 p-2">Type your email down below and be young wild generation</div>
-                  <div style={{ textAlign: "center" }} className="m-2 p-2">
-                    <input type="search" placeholder="Add your email here" style={{
-                      padding: "10px",
-                      borderRadius: "10px 0px 0px 10px",
-                      border: "none"
-                    }}
+                  <div className={`text-center ${styles.newsletterHeading}`}>JOIN SHOPPING COMMUNITY TO GET MONTHLY PROMO</div>
+                  <div className={`m-2 p-2 text-center ${styles.newsletterSubHeading}`}>Type your email down below and be young wild generation</div>
+                  <div className="m-2 p-2 text-center">
+                    <input type="search" placeholder="Add your email here" 
                       value={email}
                       onChange={handleEmailOnChange}
+                      className={styles.newsletterSubscriptionsInput}
                     />
-                    <button style={{
-                      background: "black",
-                      color: "white",
-                      borderRadius: "0px 10px 10px 0px",
-                      border: "none",
-                      paddingLeft: "20px",
-                      paddingRight: "20px",
-                      paddingTop: "10px",
-                      paddingBottom: "10px",
-                    }} onClick={() => handleNewsletterSubscription(email)}>Submit</button>
+                    <button className={styles.newsletterSubscriptionsBtn} onClick={() => handleNewsletterSubscription(email)}>Submit</button>
                     <div>
                       {message}
                     </div>
@@ -349,6 +248,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            {/* Newsletter */}
 
             {/*  Footer  */}
             <Footer></Footer>
